016f671a8c702b9bf3019c5fa024199c678cbeb6
Pavithra-RS5 <pavithraprs3@gmail.com>
2025-11-25 01:05:38 +0530

    Fix formatting in start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,4 +2,4 @@ set -e
 
 docker pull pavithrapr/sample-python-flask-app
 
-docker run -d -p 5000:5000 pavithrapr/sample-python-flask-app
+docker run -d -p 5000:5000 pavithrapr/sample-python-flask-app 
